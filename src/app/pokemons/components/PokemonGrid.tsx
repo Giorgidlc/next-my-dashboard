8df48554c45c824d9789962c52e2c4295ab2d651
Jorge <jorestuard@gmail.com>
2024-01-30 17:37:33 +0100

    create pokemons components

---
+++ src/app/pokemons/components/PokemonGrid.tsx
@@ -0,0 +1,20 @@
+import { PokemonCard } from '..';
+import { SimplePokemons } from '../interfaces/simple-pokemons'
+
+interface Props {
+  pokemons: SimplePokemons[];
+}
+
+export const PokemonGrid = ({ pokemons }: Props) => {
+  return (
+     <div className="flex flex-wrap gap-10 items-center justify-center">
+        {
+          pokemons.map(pokemon => (
+            <>
+            <PokemonCard key={pokemon.id}  pokemon={pokemon}/>
+            </>
+          ))
+        }
+      </div>
+  )
+}
--- src/app/pokemons/index.ts
+++ src/app/pokemons/index.ts
@@ -1,2 +1,5 @@
-export type { PokemonsResponse } from './interfaces/pokemons-responses'
-export type { SimplePokemons } from './interfaces/simple-pokemons'
+export type { PokemonsResponse } from './interfaces/pokemons-responses';
+export type { SimplePokemons } from './interfaces/simple-pokemons';
+
+export { PokemonGrid } from './components/PokemonGrid';
+export { PokemonCard }  from './components/PokemonCard'
