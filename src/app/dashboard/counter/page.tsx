6383ae931ffd6f209f2c6262d88bc7cf665df4b5
Jorge <jorestuard@gmail.com>
2024-01-30 12:09:39 +0100

    status management

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,25 +1,19 @@
-'use client';
-
 import { Metadata } from "next";
-import { useState } from "react";
+import { CartCounter } from "@/app/shopping-cart";
+
 
 export const metadata: Metadata = {
-  title: "Counter Page",
+  title: "Shopping Cart",
   description: "Un simple contador",
 };
 
 export default function CounterPage() {
   
-  const [counter, setCounter] = useState(3);
   
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos en el carrito de compras</span>
-      <span className="text-9xl"> {counter} </span>
-      <div className="flex gap-4">
-        <button onClick={() => setCounter(counter + 1)} className="flex p-3 items-center justify-center rounded-2xl border-blue-500 border-2 w-28 hover:bg-blue-100 transition-all">➕</button>
-        <button onClick={() => setCounter(counter - 1)} className="flex p-3 items-center justify-center rounded-2xl border-blue-500 border-2 w-28 hover:bg-blue-100 transition-all">➖</button>
-      </div>
+      <CartCounter value={ 13 } />
     </div>
   );
 }
---
+++ src/app/shopping-cart/components/CartCounter.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useState } from "react";
+
+interface Props {
+  value?: number;
+}
+
+export const CartCounter = ({ value = 0}: Props) => {
+  const [counter, setCounter] = useState(value);
+  return (
+    <>
+    <span className="text-9xl"> {counter} </span>
+      <div className="flex gap-4">
+        <button onClick={() => setCounter(counter + 1)} className="flex p-3 items-center justify-center rounded-2xl border-blue-500 border-2 w-28 hover:bg-blue-100 transition-all">➕</button>
+        <button onClick={() => setCounter(counter - 1)} className="flex p-3 items-center justify-center rounded-2xl border-blue-500 border-2 w-28 hover:bg-blue-100 transition-all">➖</button>
+      </div>
+    </>
+  )
+}
