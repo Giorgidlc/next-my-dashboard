91fe4c5e7a3bbbdfdda9b876652e4d75350e2c39
Jorge <jorestuard@gmail.com>
2024-02-08 13:08:25 +0100

    create error.tsx

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -6,7 +6,9 @@ const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemons[]> =>
   const pokemons = data.results.map(pokemon => ({
     id: pokemon.url.split('/').at(-2)!,
     name: pokemon.name,
-  }))
+  }));
+
+/*   throw new Error('Esto es un error que no debería de suceder'); */
 
   return pokemons;
 }
